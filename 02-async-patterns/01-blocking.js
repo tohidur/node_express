cadf914632ddefe0fa47512b2e96f696315e445d
tohidur <tohidur@urbanpiper.com>
2021-07-31 12:15:50 +0530

    async patterns

---
+++ 02-async-patterns/01-blocking.js
@@ -0,0 +1,25 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.end('Home page');
+  }
+
+  if (req.url === '/about') {
+    // blocking code!
+    for (let i = 0; i < 1000; i++) {
+      for (let j = 0; i < 1000; j++) {
+        console.log(`${i} - ${j}`);
+      }
+    }
+
+    res.end('About page');
+  }
+
+  res.end('Error page');
+})
+
+
+server.listen(5000, ()=>{
+  console.log('server listening on port 5000');
+})
---
+++ 02-async-patterns/03-util-and-module-promises.js
@@ -0,0 +1,27 @@
+//const { readFile, writeFile } = require('fs')
+//const util = require('util')
+//const readFilePromise = util.promisify(readFile)
+//const writeFilePromise = util.promisify(writeFile)
+
+
+const { readFile, writeFile } = require('fs').promises;
+
+
+const start = async() => {
+  try {
+    const first = await readFile('./content/first.txt', 'utf8')
+    const second = await readFile('./content/second.txt', 'utf8')
+
+    await writeFile(
+      './content/result-mind-grenade.txt',
+      `This is awesone: ${first}, ${second}`,
+      { flag: 'a' }
+    )
+
+    console.log(first, second);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+start()
