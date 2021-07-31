0274ee0b7f7eb010af19fd27195a342e52742697
tohidur <tohidur@urbanpiper.com>
2021-07-31 11:43:35 +0530

    event loop examples

---
+++ 01-event-loop-examples/01-read-file.js
@@ -0,0 +1,17 @@
+const { readFile } = require('fs');
+
+
+console.log('started the first task');
+
+
+readFile('./../content/first.txt', 'utf8', (err, result) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  console.log(result);
+  console.log('completed first task');
+})
+
+console.log('started next task');
